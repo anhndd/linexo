7a83eb7c8e7ef300209d8000acd706828a7db136
Trong Le Huu <1513696@hcmut.edu.vn>
2019-04-07 10:04:15 +0700

    add roomusecase

--- LineXO/app/src/main/java/vn/edu/hcmut/linexo/domain/interactor/RoomUsecase.java
+++ LineXO/app/src/main/java/vn/edu/hcmut/linexo/domain/interactor/RoomUsecase.java
@@ -2,14 +2,25 @@ package vn.edu.hcmut.linexo.domain.interactor;
 
 import android.support.annotation.Nullable;
 
+import io.reactivex.Single;
+import io.reactivex.SingleObserver;
+import io.reactivex.observers.DisposableSingleObserver;
+import vn.edu.hcmut.linexo.data.repository.UserRepository;
+import vn.edu.hcmut.linexo.presentation.model.User;
 import vn.edu.hcmut.linexo.utils.Event;
 
 public class RoomUsecase extends AbstractUsecase {
+    private UserRepository userRepository;
+
     @Override
     public void execute(Object observer, @Nullable int flag, @Nullable Object... params) {
         switch (flag) {
             case Event.LOGIN_INFO:
-                // TODO
+                addTask(userRepository
+                        .getCacheUser()
+                        .subscribeOn(getSubscribeScheduler())
+                        .observeOn(getObserveScheduler())
+                        .subscribeWith((DisposableSingleObserver<User>) observer));
                 break;
         }
     }
